36a46038121253eb7eb2cf7b6ea7f0ded6facd10
Niklas Bennewiz <nbennewiz@gmx.de>
2022-03-29 22:17:54 +0200

    init commit

--- Realtime/IBStrategyWithLSTM.py
+++ Realtime/IBStrategyWithLSTM.py
@@ -13,10 +13,15 @@ import math
 from datetime import datetime, timedelta
 import threading
 import time
-import torch
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+from torch.nn.modules import loss
 
-from HLSTM import Netz
+from LSTMhours import Netz
 
 #Vars
 orderId = 1
@@ -73,12 +78,12 @@ class Bar:
 #Bot Logic
 class Bot:
     ib = None
-    barsize = 5
+    barsize = 1
     currentBar = Bar()
     bars = []
     reqId = 1
     global orderId
-    symbol = "TSLA"
+    symbol = "AMZN"
     speicher = []
     IBool = True
     initialbartime = datetime.now().astimezone(pytz.timezone("America/New_York"))
@@ -103,11 +108,17 @@ class Bot:
         contract.currency = "USD"
         self.ib.reqIds(-1)
         # Request Market Data
-        #self.ib.reqRealTimeBars(0, contract, 5, "TRADES", 1, [])
-        self.ib.reqHistoricalData(self.reqId,contract,"","3 D",str(self.barsize)+mintext,"TRADES",1,1,True,[])
-        #self.ib.reqHistoricalData(self.reqId, contract, "", )
-        #Load all Models and Signals
-        checkpoint = torch.load(r"C:\Users\jensb\Documents\Praktikum\Stock_Prediction\03-07-2021\JugendForscht\deepLearning\PricePredictionModel_hourly.pt")
+        self.ib.reqHistoricalData(self.reqId,contract,"","3 D", "1 hour","TRADES",1,1,True,[])
+        
+        #load moving range model
+        ampel_signals = pd.read_csv(r"C:\Users\jensb\Documents\Praktikum\Stock_Prediction\03-07-2021\Börsenampel\Table\ampelCSVexport_29032022.csv")
+        self.movingRange = ampel_signals["Results-Percent"].to_list()[-1]
+
+        #Load deep learning model
+        self.ss = StandardScaler()
+        self.mm = MinMaxScaler(feature_range=(0,1))
+
+        checkpoint = torch.load(r"C:\Users\jensb\Documents\Praktikum\Stock_Prediction\03-07-2021\JugendForscht\deepLearning\PricePredictionModel_minutes.pt")
         self.learning_rate = 0.0005
         self.inputs = 5
         self.hiddens = 60
@@ -115,11 +126,20 @@ class Bot:
         self.outputs = 1
         self.seq_length = 5
 
+        self.predictions = []
+        self.predictionsToData = []
+        self.first = True
+        self.lossData = []
+
         self.model = Netz(self.outputs, self.inputs, self.hiddens, self.layers, 5)
         self.model.load_state_dict(checkpoint["model_state_dict"])
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
         self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
+        self.criterion = nn.MSELoss()
+
+        self.model.train()
 
         print("Model's state_dict:")
         for param_tensor in self.model.state_dict():
@@ -129,6 +149,8 @@ class Bot:
         for var_name in self.optimizer.state_dict():
             print(var_name, "\t", self.optimizer.state_dict()[var_name])
         '''
+        
+
 
 
     #Listen to socket in seperate thread
@@ -183,7 +205,7 @@ class Bot:
             self.bars.append(bar)
         else:
             bartime = datetime.strptime(bar.date,"%Y%m%d %H:%M:%S").astimezone(pytz.timezone("America/New_York"))
-            minutes_diff = (bartime-self.initialbartime).total_seconds() / 60.0
+            minutes_diff = (bartime-self.initialbartime).total_seconds() / 3600.0#One hour
             self.currentBar.date = bartime
             lastBar = self.bars[len(self.bars)-1]
             #On Bar Close
@@ -195,42 +217,134 @@ class Bot:
                 highs = []
                 lows =[]
                 closes = []
+                volumes = []
+                dates = []
                 
                 for entry in self.bars:
                     opens.append(entry.open)
                     highs.append(entry.high)
                     lows.append(entry.low)
                     closes.append(entry.close)
+                    volumes.append(entry.volume)
+                    dates.append(entry.date)
                 self.open_array = pd.Series(np.asarray(opens))
                 self.high_array = pd.Series(np.asarray(highs))
                 self.low_array = pd.Series(np.asarray(lows))
                 self.close_array = pd.Series(np.asarray(closes))
-
+                self.volume_array = pd.Series(np.asarray(volumes))
+                self.date_array = pd.Series(np.asarray(dates))
+                
                 #Create realtime Bar
                 thisOpen = opens[-1]
                 thisLow = lows[-1]
                 thisHigh = highs[-1]
                 thisClose = closes[-1]
+                thisVolume = volumes[-1]
+                thisDate = dates[-1]
 
                 #Create previous Bar
                 prevOpen = opens[-2]
                 prevLow = lows[-2]
                 prevHigh = highs[-2]
                 prevClose = closes[-2]
+                prevVolume = volumes[-2]
+                prevDate = dates[-2]
 
                 #Print realtime/this Bar and previous Bar
-                '''
+                
                 print(" ")
-                print("This Bar: ", "Open:", thisOpen, " High: ", thisHigh, " Low:", thisLow, " Close:",thisClose)
-                print("Previous Bar: ", "Open:", prevOpen, " High: ", prevHigh, " Low:", prevLow, " Close:",prevClose)
-                '''
+                print("This Bar: ", " Date:",thisDate, "Open:", thisOpen, " High: ", thisHigh, " Low:", thisLow, " Close:",thisClose, " Volume:",thisVolume)
+                print("Previous Bar: ", " Date:", prevDate, "Open:", prevOpen, " High: ", prevHigh, " Low:", prevLow, " Close:",prevClose, " Volume:",prevVolume)
+                
+                df = pd.DataFrame()
+                df["open"] = self.open_array
+                df["low"] = self.low_array
+                df["high"] = self.high_array
+                df["close"] = self.close_array
+                df["volume"] = self.volume_array
+                #df["date"] = self.date_array
+                
+                
+                if self.first != True:
+                    y_tensor = self.mm.fit_transform(df["close"][-1])
+                    loss = self.criterion(predictions[-1], y_tensor)
+                    loss.backward()
+                    self.optimizer.step()
+                    self.lossData.append(loss.item())
+
+                self.optimizer.zero_grad()
+
+                #for fitting
+                fit = pd.DataFrame(df["close"][:-1])
+                self.mm.fit_transform(fit)
+
+                X_ss = self.ss.fit_transform(df)
+
+                X_batches = []
+                for i in range(len(X_ss)-2, len(X_ss)):
+                    X_batches.append(X_ss[i-5:i])
+
+                X_tensor = Variable(torch.Tensor(X_batches))
+
+                predictions = self.model(X_tensor)
+                self.predictions.append(predictions[-1])
+                predictionsToData = self.mm.inverse_transform(predictions.data.numpy()).flatten().tolist()
+                print(predictionsToData)
+                self.predictionsToData.append(predictionsToData[-1])
+                diff = predictionsToData[-1] - predictionsToData[-2] 
+                print(diff)
+                
+                self.first=False
                 
                 # Check Buy Criteria
-               
+                if diff > 0:#Buy
+                    #Bracket Order 2% Profit Target 1% Stop Loss
+                    profitTarget = bar.close*1.02
+                    stopLoss = bar.close*0.99
+                    quantity = 1
+                    bracket, self.lastOrderID = self.bracketOrder(orderId,"BUY",quantity, profitTarget, stopLoss)
+                    contract = Contract()
+                    contract.symbol = self.symbol.upper()
+                    contract.secType = "STK"
+                    contract.exchange = "SMART"
+                    contract.currency = "USD"
+                    #Place Bracket Order
+                    for o in bracket:
+                        o.ocaGroup = "OCA_"+str(orderId)
+                        self.ib.placeOrder(o.orderId,contract,o)
+                    orderId += 3
+                    print("Order placing...")
+                    print("OrderID (ParentOrderID): ", orderId-3, "Type: SELL - market", "StopLoss placed at ", stopLoss, "ProfitTarget placed at ", profitTarget)
+                
+                elif diff < 0:#Sell 
+                    #Bracket Order 2% Profit Target 1% Stop Loss
+                    profitTarget = bar.close*0.98
+                    stopLoss = bar.close*1.01
+                    quantity = 1
+                    bracket, self.lastOrderID = self.bracketOrder(orderId,"SELL",quantity, profitTarget, stopLoss)
+                    contract = Contract()
+                    contract.symbol = self.symbol.upper()
+                    contract.secType = "STK"
+                    contract.exchange = "SMART"
+                    contract.currency = "USD"
+                    #Place Bracket Order
+                    for o in bracket:
+                        o.ocaGroup = "OCA_"+str(orderId)
+                        self.ib.placeOrder(o.orderId,contract,o)
+                    orderId += 3
+                    print("Order placing...")
+                    print("OrderID (ParentOrderID): ", orderId-3, "Type: SELL - market", "StopLoss placed at ", stopLoss, "ProfitTarget placed at ", profitTarget)
+                    
 
-                #Ask Models
-
-                #For the Order add the Indicators
+                '''
+                try:
+                    self.ib.cancelOrder(self.lastOrderID)
+                    self.ib.cancelOrder(self.lastOrderID+1)
+                    self.ib.cancelOrder(self.lastOrderID+2)
+                    print("Order storniert (buy)")
+                except Exception as e:
+                    print(e)
+                '''
 
                 #Nicht Ändern Sonst funktioniert das system nicht mehr
                 #Bar closed append
